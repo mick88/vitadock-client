5113ddf685eb5ceb5ebad881d0a676dde7b0cb15
Michal <michal@maithu.com>
2015-09-09 12:10:08 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='vitadock-client',
+    version='0.1',
+    packages=['vitadock_client'],
+    url='https://github.com/mick88/vitadock-client',
+    license='MIT',
+    author='Michal Dabski',
+    author_email='michal@maithu.com',
+    description='Vitadock API client for Python'
+)
